087f562f56870e0b8126337cee3599b62915fd78
z5513054 <z5513054@ad.unsw.edu.au>
2024-06-09 16:04:45 +1000

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello {
-  public static void main(string[] args) {
+  public static void main(String[] args) {
     //displays "hello everyone" in console
-  system.out.println{"hello everyone");
+  System.out.println{"hello everyone");
                     }
   }
